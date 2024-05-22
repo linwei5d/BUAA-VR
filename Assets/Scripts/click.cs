4881b1678f49101bf6c2099a9ebf01474f3909dd
linwei5d <171436671@qq.com>
2024-05-22 14:11:13 +0800

    Initial

---
+++ Assets/Scripts/click.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("start");
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log("Button Clicked!");
+        // 在这里编写点击事件的逻辑
+    }
+
+}
